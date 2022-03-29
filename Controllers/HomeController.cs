009ada0241beb95de799686560e639d127498af3
cortex225 <48181439+cortex225@users.noreply.github.com>
2022-03-29 15:41:41 -0400

    Partie1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,13 @@ namespace Exercice_Ajax.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public IActionResult Partie1()
+        {
+            return (NoContent());
+        }
+
+
     }
 }
